19c2783bcbb06c002ac7360510f53f822ffddef7
Martin Martiniak <martiniak.martin15@gmail.com>
2021-05-25 19:26:45 +0200

    Add objects Lesson and Subject

---
+++ SchoolReminder/SchoolReminder/Models/Lesson.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolReminder.Models
+{
+    class Lesson
+    {
+        public string Zaciatok { get; set; }
+        public string Koniec { get; set; }
+
+        public Lesson(string zaciatok, string koniec)
+        {
+            Zaciatok = zaciatok;
+            Koniec = koniec;
+        }
+        public Lesson()
+        {
+            Zaciatok = "-1";
+            Koniec = "-1";
+        }
+        public override string ToString()
+        {
+            return $"\nMeno:{Zaciatok}\nUcitel:{Koniec}";
+        }
+    }
+}
---
+++ SchoolReminder/SchoolReminder/Models/Subject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolReminder.Models
+{
+    class Subject
+    {
+        public string Meno { get; set; }
+        public string Ucitel { get; set; }
+        public string Trieda { get; set; }
+
+
+        public Subject(string meno, string ucitel, string trieda)
+        {
+            Meno = meno;
+            Ucitel = ucitel;
+            Trieda = trieda;
+        }
+        public Subject()
+        {
+            Meno = "-1";
+            Ucitel = "-1";
+            Trieda = "-1";
+        }
+        public override string ToString()
+        {
+            return $"\nMeno:{Meno}\nUcitel:{Ucitel}\nTrieda:{Trieda}";
+        }
+    }
+}
